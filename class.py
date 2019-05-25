f1fbf7d152354a2cf1daa06f52ac5e0f10d32751
akshat <akshatgoyalak23@gmail.com>
2019-05-26 00:25:45 +0530

    Shooting game

---
+++ class.py
@@ -0,0 +1,14 @@
+x = 'string'
+y = 23
+boo = True
+
+class number():
+	def __init__(self,num):
+		self.var=num
+
+	def display(self,x):
+		print(x)
+
+num = number(23)
+num.display(num.var);
+# print(num)
